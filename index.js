2fb6639cfd5e1cf9d3dace28eeab56e5f824d690
Jny <nerdrun.jny@gmail.com>
2018-10-15 14:46:20 +0900

    #1 setting - connected mongodb

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://localhost:27017'
+const dbName = 'test'
 
 app.set('PORT', 3000)
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify({ text: 'hello world' }))
+  const user = { id: 'hello', pw: 'password' }
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName)
+    db.collection('users').insertOne(user, (err, result) => {
+      if(err) res.send({ err: 'fuck!' })
+
+      res.send(result)
+      client.close()
+
+    })
+  })
 })
 
 app.listen(app.get('PORT'), () => {
