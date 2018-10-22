70b4f2b14260df66f852747e204d0963badf299d
Jny <nerdrun.jny@gmail.com>
2018-10-22 16:40:19 +0900

    #3 done profile list && doing profile detal

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
 
 const url = 'mongodb://localhost:27017'
 const dbName = 'profile'
@@ -13,6 +14,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName)
+    db.collection('users').find().toArray()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ msg: 'err'}))
+    client.close()
+  })
 })
 
 app.post('/', (req, res) => {
@@ -21,6 +29,19 @@ app.post('/', (req, res) => {
   MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     const db = client.db(dbName)
     db.collection('users').insertOne(userInfo)
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ msg: 'err'}))
+    client.close()
+  })
+})
+
+/* get user infomation from mongodb */
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  const obj_id = new ObjectId(id)
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName)
+    db.collection('users').findOne({ _id: obj_id })
     .then(data => res.json(data))
     .catch(err => res.status(500).json({ msg: 'err'}))
     client.close()
