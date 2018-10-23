18b4fb9429f6121cfcffa707abd2c35b08e37ec7
Jny <nerdrun.jny@gmail.com>
2018-10-24 03:24:16 +0900

    #3 done profile detail && profile modify

--- index.js
+++ index.js
@@ -47,7 +47,63 @@ app.get('/:id', (req, res) => {
     client.close()
   })
 })
+/* go to update form */
+app.get('/edit/:id', (req, res) => {
+  const { id } = req.params
+  const obj_id = new ObjectId(id)
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName)
+    db.collection('users').findOne({ _id: obj_id })
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ msg: 'err'}))
+    client.close()
+  })
+})
+app.put('/:id', (req, res) => {
+  const { id } = req.params
+  const objId = new ObjectId(id)
+  const userInfo = req.body
+  console.log(userInfo._id)
 
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName)
+    db.collection('users').updateOne(
+      { _id: objId },
+      {
+        $set: {
+          image: userInfo.image,
+          category: userInfo.category,
+          name: userInfo.name,
+          mobile: userInfo.mobile,
+          email: userInfo.email,
+          birthDay: userInfo.birthDay,
+          sex: userInfo.sex,
+          age: userInfo.age,
+          blood: userInfo.blood,
+          job: userInfo.job,
+          hobby: userInfo.hobby,
+          nickName: userInfo.nickName,
+          religion: userInfo.religion,
+          userAddress: {
+            address: userInfo.userAddress.address,
+            street: userInfo.userAddress.street,
+            zipCode: userInfo.userAddress.zipCode
+          },
+          company: {
+            name: userInfo.company.name,
+            departments: userInfo.company.departments,
+            position: userInfo.company.position
+          },
+          character: userInfo.character
+      }
+    })
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ msg: 'err'}))
+    client.close()
+  })
+
+
+})
 app.listen(app.get('PORT'), () => {
   console.log(` server started ... PORT: ${app.get('PORT')} `)
 })
